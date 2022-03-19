9b73c8140c8c4ac5e45ed16fa7f715d2b1c99204
G Y <97574449+apircity@users.noreply.github.com>
2022-03-19 18:18:27 +0900

    Add files via upload

---
+++ 1046_정수 3개 합 평균.c
@@ -0,0 +1,18 @@
+﻿
+#include <stdio.h>
+ 
+ 
+int main (void)
+{
+    int num1, num2, num3, sum;
+    float avr;
+    
+    scanf("%d %d %d", &num1, &num2, &num3);
+    sum = num1 + num2 + num3;
+    avr = (float)sum/3;
+    
+    printf("%d\n",sum);
+    printf("%.01f",avr);
+    
+    return 0;
+}
---
+++ 1051_a b 대소비교.c
@@ -0,0 +1,9 @@
+﻿#include<stdio.h>
+int main()
+{
+    long long int a,b;
+    scanf("%lld %lld", &a,&b);
+    printf("%d", a<=b);
+    return 0;
+}
+
