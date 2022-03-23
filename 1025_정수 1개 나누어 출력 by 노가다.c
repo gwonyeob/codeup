7619ce7bbbf27c8a3dcd7cd6dc1f3e0a0376264e
G Y <97574449+apircity@users.noreply.github.com>
2022-03-24 01:33:54 +0900

    Add files via upload

---
+++ 1025_정수 1개 나누어 출력 by 노가다.c
@@ -0,0 +1,13 @@
+﻿#include<stdio.h>
+int main()
+{
+    int a, b, c, d, e, zero;
+    zero=0;
+    scanf("%1d%1d%1d%1d%1d", &a, &b, &c, &d, &e);
+    printf("[%1d%1d%1d%1d%1d]\n", a, zero, zero, zero, zero);
+    printf("[%1d%1d%1d%1d]\n", b, zero, zero, zero);
+    printf("[%1d%1d%1d]\n", c, zero, zero);
+    printf("[%1d%1d]\n", d, zero);
+    printf("[%1d]",e);
+    return 0;
+}
