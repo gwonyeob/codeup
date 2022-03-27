cfc515af6b76c4ce632f1590eb986ab24a9f1c60
G Y <97574449+apircity@users.noreply.github.com>
2022-03-27 13:12:12 +0900

    Add files via upload

---
+++ 1069_평가 다르게 입력.c
@@ -0,0 +1,25 @@
+﻿#include<stdio.h>//jumsu:평가
+int main()
+{
+	char jumsu;
+	scanf("%c", &jumsu);
+	switch (jumsu)
+	{
+	case 'A':
+		printf("%s","best!!!");
+			break;
+	case 'B':
+		printf("%s", "good!!");
+			break;
+	case 'C':
+		printf("%s", "run!");
+			break;
+	case 'D':
+		printf("%s","slowly~");
+			break;
+	default:
+	    printf("%s", "what?");
+		return 0;
+	}
+
+}
---
+++ 1070_계절 출력.c
@@ -0,0 +1,29 @@
+﻿#include<stdio.h>//월 입력->계절 출력
+int main()
+{
+	int season;
+	scanf("%d", &season);
+	switch (season)
+	{
+	case 12:
+	case 1:
+	case 2:			
+		printf("winter");
+		break;
+	case 3:
+	case 4:
+	case 5:
+		printf("spring");
+		break;
+	case 6:
+	case 7:
+	case 8:
+		printf("summer");
+		break;
+	case 9:
+	case 10:
+	case 11:
+		printf("fall");
+	    break;
+	}
+}
